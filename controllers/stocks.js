6996006863bcffaa179497e8a0aba8e122df26d7
rtaladua <rtaladua@gmail.com>
2023-03-15 19:51:12 -0700

    making some type of progress lol

--- controllers/stocks.js
+++ controllers/stocks.js
@@ -19,21 +19,45 @@ const db = require('../models')
 /* Routes
 --------------------------------------------------------------- */
 // Index Route (GET/Read): Will display all stocks
+// router.get('/', function (req, res) {
+//     db.Stock.find({})
+//         .then(stocks => res.json(stocks))
+// })
+
 router.get('/', function (req, res) {
     db.Stock.find({})
-        .then(stocks => res.json(stocks))
+    .then(stocks => {
+        res.render('stock-index', {
+            stocks: stocks
+        })
+    })
 })
 
+
+// router.get('/:id', function (req, res){
+//     db.Stock.findById(req.params.id)
+//     .then(stocks => {
+//         res.render('stock-detail', {
+//             stocks: stocks
+//         })
+//     })
+// })
 
 // Show Route (GET/Read): Will display an individual stock document
 // using the URL parameter (which is the document _id)
 router.get('/:id', function (req, res) {
     db.Stock.findById(req.params.id)
-        .then(stock => res.json(stock))
-        .catch(() => res.send('404 Error: Page Not Found'))
+        .then(stocks => {
+            res.render('stock-detail', {
+                stock: stocks
+            })
+        })
+            .catch(() => res.send('404 Error: Gary Rocks'))
 })
+
 
 
 /* Export these routes so that they are accessible in `server.js`
 --------------------------------------------------------------- */
 module.exports = router
+
